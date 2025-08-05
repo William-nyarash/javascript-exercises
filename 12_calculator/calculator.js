d230ae7ce7432f7feb438542ea656e30b06bf580
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-05 23:53:45 +0300

    add calculator logic

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b ) {
+	return a + b ;
 };
 
-const subtract = function() {
-	
+const subtract = function( a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr ) {
+	const arraySum = arr.reduce((a,b) => {
+      return a + b
+  }, 0)
+  return arraySum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    const multiplied = arr.reduce((a, b) => {
+        return a * b;
+    })
+    return multiplied;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	 return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) return undefined;
+
+  let value = 1;
+  let number = 2;
+  for ( ; number <= n ; number++) {
+    value *= number;
+  }
+  return value;
 };
 
 // Do not edit below this line
