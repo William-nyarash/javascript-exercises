da8ded97738e3ced54d15991c114cf55ff51f686
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-06 00:21:29 +0300

    impliment a palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (args) {
+    const palindrome = args.toLowerCase().replace(/[^a-z0-9]/g, '');    
+    const cleaned = palindrome.split("").reverse().join('')
+    if (cleaned === palindrome) return true;
 
+    return false;
 };
 
 // Do not edit below this line
